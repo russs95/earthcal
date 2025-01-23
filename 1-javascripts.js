64900acc172701571e498ef45ca6f66314c5ed80
Russell Maier <russ@earthen.io>
2025-01-23 20:03:04 +0700

    j

--- 1-javascripts.js
+++ 1-javascripts.js
@@ -368,7 +368,7 @@ async function openAddCycle() {
 
 
 
-async function populateCalendarDropdown(buwanaId) {
+function populateCalendarDropdown(buwanaId) {
     console.log('populateCalendarDropdown called with buwanaId:', buwanaId);
 
     const calendarDropdown = document.getElementById('select-calendar');
