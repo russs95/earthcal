0d912f649fc06ef41b0ce3c3e806b38e40724ab7
Russell Maier <russ@earthen.io>
2025-02-02 23:24:32 +0700

    comon doug

--- 1-event-management.js
+++ 1-event-management.js
@@ -528,7 +528,7 @@ function writeMatchingDateCycles(divElement, dateCycle) {
             role="button"
             aria-label="Push to today"
             title="Push to today"
-            onclick="('${dateCycle.unique_key}'); event.stopPropagation();"
+            onclick="push2today('${dateCycle.unique_key}'); event.stopPropagation();"
             style="font-size: larger; cursor: pointer; background: none; border: none;">
             ➜
         </button>`;
