bb131babd9db62aaae8368b57aff98c9dd0f39e5
ubuntusitubondo <russ@earthen.io>
2025-10-20 20:35:29 +0700

    New dash-2 for svg delegated dev

--- js/earthcal-init.js
+++ js/earthcal-init.js
@@ -57,7 +57,7 @@ async function initCalendar() {
         "js/breakouts.js",
         "js/set-targetdate.js",
         "js/planet-orbits.js",
-        "js/login-scripts.js?v=10",
+        "js/login-scripts.js?v=10.1",
         "js/item-management.js?v=7",
         "js/time-setting.js",
         "js/calendar-scripts.js?v=1",
