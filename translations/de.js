37a35e83342f280523c5b2469a866da7e5853e23
Russell Maier <68637813+russs95@users.noreply.github.com>
2025-10-31 17:36:38 +0700

    Revise subscription modal copy and styling (#184)

--- translations/de.js
+++ translations/de.js
@@ -159,7 +159,7 @@ mainMenu: {
 },
 
 subscriptions: {
-  heading: "Select Moment Mastery",
+  heading: "Upgrade EarthCal",
   currentPlan: "You are currently on the {planName} plan.",
   currentStatus: "Status: {status}",
   loginRequired: "Please sign in to manage your EarthCal subscription.",
